eaea927d5d9c0b770ca2d84f4de089b4464e32ab
Byra Manikanta Prasad <manikantabyra@gmail.com>
2025-10-03 09:01:38 +0530

    Delete Bank Management System directory

--- Bank Management System/admin/view_all_accounts.py
+++
@@ -1,9 +0,0 @@
-from admin.admin_login import admin_login
-from data import customers
-
-def view_all_accounts():
-    
-    print("--- Accounts ---")
-    for cust in customers:
-        print(f"ID: {cust['id']}, Name: {cust['name']}, Username: {cust['username']}, Balance: {cust['balance']}rs")
-    print()
--- Bank Management System/customer/check_balance.py
+++
@@ -1,8 +0,0 @@
-from utils.find_customer import find_customer
-
-def check_balance(user_entered, pin_entered):
-    customer = find_customer(user_entered, pin_entered)
-    if customer:
-        print(f"\nYour current balance is {customer['balance']}rs")
-    # else:
-    #     print("Invalid credentials.\n")
